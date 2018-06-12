36d96716322e42b2b1cf8f889b6580167c16c96e
gustavo <gustavods07@gmail.com>
2018-06-12 16:54:31 -0300

    segundo commit

--- app/src/main/java/com/example/gustavo/aplicativo/MainCadastro.java
+++ app/src/main/java/com/example/gustavo/aplicativo/MainCadastro.java
@@ -5,11 +5,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.gson.JsonObject;
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
 
 public class MainCadastro extends AppCompatActivity {
 
     private EditText editNomeCad, editEmailCad, editSenhaCad, editSenhaConf;
     private Button btnCadastrar;
+    private String HOST = "http://10.15.10.70/login";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +32,46 @@ public class MainCadastro extends AppCompatActivity {
         btnCadastrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+            String nome = editNomeCad.getText().toString();
+            String email = editEmailCad.getText().toString();
+            String senha = editSenhaCad.getText().toString();
+            String confirma = editSenhaConf.getText().toString();
+
+            String URL =  HOST + "/cadastrar.php";
+
+            if(confirma.equals(senha)){
+
+
+                Ion.with(MainCadastro.this)
+                        .load(URL)
+                        .asJsonObject()
+                        .setCallback(new FutureCallback<JsonObject>() {
+                            @Override
+                            public void onCompleted(Exception e, JsonObject result) {
+
+                                try{
+
+                                    Toast.makeText(MainCadastro.this, "Nome: " + result.get("NOME").getAsString(), Toast.LENGTH_LONG).show();
+
+
+                                } catch (Exception erro ) {
+
+                                    Toast.makeText(MainCadastro.this, "Ops, ocorreu um erro, " + erro, Toast.LENGTH_LONG).show();
+
+                                }
+                            }
+                        });
+
+
+            } else {
+
+                Toast.makeText(MainCadastro.this, "As senhas não conferem", Toast.LENGTH_LONG).show();
+
+            }
+
+
+
 
             }
         });
